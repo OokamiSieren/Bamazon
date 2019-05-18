e3d8b58218332713802e9c01e0cf7a6933ef06e9
Diana <d_quinlan061006@yahoo.com>
2019-05-17 23:58:10 -0400

    updated readme

---
+++ README.md
@@ -0,0 +1,4 @@
+This is the Bamazon application.
+View the available items and select them via thier id number and how many you would like to make a puchase. You will get a total for the items as well as availabity if the quantity given is more than what is in stock. Here is a link for a video demonstration: https://drive.google.com/file/d/1bcusVm7ewyC7TttSViJ3b4myG7N9EFrp/view
+
+Here is a link to view screenshots: https://drive.google.com/open?id=119MJ-YXu1rFCcUNP6Z4RGGrx6sGEjDan
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,14 +8,16 @@ var connection = mysql.createConnection({
   database: "bamazondb"
 });
 
-// var quantity;
-
-//step one display products in table
 //connect to database
 connection.connect(function(err) {
   if (err) throw err;
   console.log("Welcome to Bamazon!" + "\n");
 });
+
+
+//step one display products in table
+
+
 // display table
 function displayTable() {
   connection.query("SELECT * FROM products", function(error, results) {
@@ -33,15 +35,14 @@ function displayTable() {
           line
       );
     }
-
+//call purchase function
     purchase();
-    // connection.end();
+   
   });
 }
+
 displayTable();
-// step two The app should then prompt users with two messages.
-// The first should ask them the ID of the product they would like to buy.
-// The second message should ask how many units of the product they would like to buy.
+
 function purchase() {
   inquirer
     .prompt([
@@ -71,45 +72,28 @@ function purchase() {
           console.log("Your item: " + results[0].product_name + " for $"+ results[0].price);
           
           if (answer.quantity <= results[0].stock_quantity) {
-            connection.query(
+             connection.query(
               "UPDATE products SET stock_quantity = ? WHERE id = ?",
-              [
-                {
-                  stock_quantity: (results[0].stock_quantity - parseInt(answer.quantity))
-                }
-              ],
+               [
+                 {
+                   stock_quantity: (results[0].stock_quantity - parseInt(answer.quantity))
+                 }
+               ],
         console.log("Your total is $ " + answer.quantity * results[0].price.toFixed(2) + " dollars. Thank you for your purchase!")
-            );
+             );
               
           } else {
-            console.log("Insufficent quantity!");
+            console.log("Insufficent quantity! We only have " + results[0].stock_quantity + ".");
           }
-      
+     
     });
+    
   }
-  )}; 
-// new function for inventory
-//step three check the inventory and let user know if item is available
-// If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
-// if your store does have enough of the product, you should fulfill the customer's order.
-// This means updating the SQL database to reflect the remaining quantity.
-// Once the update goes through, show the customer the total cost of their purchase.
-// function inventory(answer) {
-//   if (answer.quantity <= stock_quantity) {
-//     connection.query(
-//       "UPDATE products SET ? WHERE ?",
-//       [
-//         {
-//           stock_quantity: (quantity - parseInt(stock_quantity))
-//         }
-//       ],
 
-//       console.log("Thank You for your purchase!")
-//     );
-//   } else {
-//     console.log("Insufficent quantity!");
-//   }
-// }
+  )}; 
+    
+
+  
   
     
   
